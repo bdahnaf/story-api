16b393b2f13c615ebbf6230b80095cb9becf5e14
Ahnaf Shahriar <Ahnaf Shahriar@AHNAF-PC>
2018-07-12 00:59:28 +0600

    Added Model Class for Story Table

---
+++ BusinessLayer/Story.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+     public class Story
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string PostedDate { get; set; }
+    }
+}
