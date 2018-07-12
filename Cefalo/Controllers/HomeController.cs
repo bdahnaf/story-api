eaa19e01b997edeb3e56fd90f05fab3cc4367f61
Ahnaf Shahriar <Ahnaf Shahriar@AHNAF-PC>
2018-07-12 23:25:23 +0600

    Added View for creating new story

--- Cefalo/Controllers/HomeController.cs
+++ Cefalo/Controllers/HomeController.cs
@@ -21,8 +21,21 @@ namespace Cefalo.Controllers
         [HttpGet]
         public ActionResult Create()
         {
-            
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollectionObj)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (string key in formCollectionObj.AllKeys)
+                {
+                    Response.Write("Key = " + key + "  ");
+                    Response.Write("Value = " + formCollectionObj[key]);
+                    Response.Write("<br/>");
+                }
+            }
             return View();
         }
     }
