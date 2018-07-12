e4d011a4b2ab8fda7602f0b5a9e9dc2d35436f1d
Ahnaf Shahriar <Ahnaf Shahriar@AHNAF-PC>
2018-07-12 18:12:02 +0600

    Added Bootstrap & Jquery, Modified Story View

--- Cefalo/Controllers/HomeController.cs
+++ Cefalo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer;
 
 namespace Cefalo.Controllers
 {
@@ -11,7 +12,10 @@ namespace Cefalo.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            StoryBusinessLayer storyBusinessLayerObj = new StoryBusinessLayer();
+            List<Story> storiesObj = storyBusinessLayerObj.Stories.ToList();
+
+            return View(storiesObj);
         }
     }
 }
