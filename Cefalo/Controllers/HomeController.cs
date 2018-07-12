903dec9b73f3196462ff41b4a9842b4f7cdd9ce2
Ahnaf Shahriar <Ahnaf Shahriar@AHNAF-PC>
2018-07-13 00:45:34 +0600

    Added wrong user input prevention method

--- Cefalo/Controllers/HomeController.cs
+++ Cefalo/Controllers/HomeController.cs
@@ -19,24 +19,39 @@ namespace Cefalo.Controllers
         }
 
         [HttpGet]
-        public ActionResult Create()
+        [ActionName("Create")]
+        public ActionResult Create_Get()
         {
             return View();
         }
 
+        //[HttpPost]
+        //public ActionResult Create(FormCollection formCollectionObj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        Story storyObj = new Story();
+        //        storyObj.Title = formCollectionObj["Title"];
+        //        storyObj.Body = formCollectionObj["Body"];
+        //        storyObj.DatePosted = formCollectionObj["DatePosted"];
+        //        StoryBusinessLayer storyBusinessLayerObj = new StoryBusinessLayer();
+        //        storyBusinessLayerObj.AddStory(storyObj);
+        //    }
+        //    return RedirectToAction("Index");
+        //}
+
+
         [HttpPost]
-        public ActionResult Create(FormCollection formCollectionObj)
+        [ActionName("Create")]
+        public ActionResult Create_Post(Story storyObj)
         {
             if (ModelState.IsValid)
             {
-                Story storyObj = new Story();
-                storyObj.Title = formCollectionObj["Title"];
-                storyObj.Body = formCollectionObj["Body"];
-                storyObj.DatePosted = formCollectionObj["DatePosted"];
                 StoryBusinessLayer storyBusinessLayerObj = new StoryBusinessLayer();
                 storyBusinessLayerObj.AddStory(storyObj);
+                return RedirectToAction("Index");
             }
-            return RedirectToAction("Index");
+            return View();
         }
     }
 }
