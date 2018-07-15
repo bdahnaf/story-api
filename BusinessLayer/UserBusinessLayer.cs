6220f5224dab71eb03dfe999c11a574700def41e
Ahnaf Shahriar <Ahnaf Shahriar@AHNAF-PC>
2018-07-15 21:37:19 +0600

    Added business layer for creating new user

---
+++ BusinessLayer/UserBusinessLayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class UserBusinessLayer
+    {
+        public void AddUser(User userssObj)
+        {
+            string connectionString =
+            ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddUser", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramUsername = new SqlParameter();
+                paramUsername.ParameterName = "@vUsername";
+                paramUsername.Value = userssObj.username;
+                cmd.Parameters.Add(paramUsername);
+
+                SqlParameter paramPassword = new SqlParameter();
+                paramPassword.ParameterName = "@vPassword";
+                paramPassword.Value = userssObj.password;
+                cmd.Parameters.Add(paramPassword);
+
+                SqlParameter paramFullName = new SqlParameter();
+                paramFullName.ParameterName = "@vFullName";
+                paramFullName.Value = userssObj.fullname;
+                cmd.Parameters.Add(paramFullName);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
