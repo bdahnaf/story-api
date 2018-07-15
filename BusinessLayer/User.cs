94606bf53dd89bf88e97ded0ceb4755f3cf7aec6
Ahnaf Shahriar <Ahnaf Shahriar@AHNAF-PC>
2018-07-15 21:30:59 +0600

    Added User model to reflect DB

---
+++ BusinessLayer/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLayer
+{
+    public class User
+    {
+        public int ID { get; set; }
+        [Required]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string fullname { get; set; }
+    }
+}
