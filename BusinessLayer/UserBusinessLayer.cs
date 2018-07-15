53f28bf94c72cc88796fc736fd57c3958fffa56b
Ahnaf Shahriar <Ahnaf Shahriar@AHNAF-PC>
2018-07-15 23:45:03 +0600

    Implemented user validity check

--- BusinessLayer/UserBusinessLayer.cs
+++ BusinessLayer/UserBusinessLayer.cs
@@ -11,7 +11,7 @@ namespace BusinessLayer
 {
     public class UserBusinessLayer
     {
-        public void AddUser(User userssObj)
+        public void AddUser(User usersObj)
         {
             string connectionString =
             ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
@@ -23,22 +23,53 @@ namespace BusinessLayer
 
                 SqlParameter paramUsername = new SqlParameter();
                 paramUsername.ParameterName = "@vUsername";
-                paramUsername.Value = userssObj.username;
+                paramUsername.Value = usersObj.username;
                 cmd.Parameters.Add(paramUsername);
 
                 SqlParameter paramPassword = new SqlParameter();
                 paramPassword.ParameterName = "@vPassword";
-                paramPassword.Value = userssObj.password;
+                paramPassword.Value = usersObj.password;
                 cmd.Parameters.Add(paramPassword);
 
                 SqlParameter paramFullName = new SqlParameter();
                 paramFullName.ParameterName = "@vFullName";
-                paramFullName.Value = userssObj.fullname;
+                paramFullName.Value = usersObj.fullname;
                 cmd.Parameters.Add(paramFullName);
 
                 con.Open();
                 cmd.ExecuteNonQuery();
             }
+        }
+
+        public bool isUser(User usersObj)
+        {
+            bool isValidUser = false;
+            string connectionString =
+            ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spCheckUser", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramUsername = new SqlParameter();
+                paramUsername.ParameterName = "@vUsername";
+                paramUsername.Value = usersObj.username;
+                cmd.Parameters.Add(paramUsername);
+
+                SqlParameter paramPassword = new SqlParameter();
+                paramPassword.ParameterName = "@vPassword";
+                paramPassword.Value = usersObj.password;
+                cmd.Parameters.Add(paramPassword);
+
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    isValidUser = true;
+                }
+            }
+            return isValidUser;
         }
     }
 }
