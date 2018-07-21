f8df00c77fa695e3971c9b3b19e31deda6975aca
Ahnaf <bdahnaf@gmail.com>
2018-07-21 19:30:52 +0600

    Added confirm password functionalities

--- BusinessLayer/User.cs
+++ BusinessLayer/User.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BusinessLayer
 {
@@ -11,10 +12,17 @@ namespace BusinessLayer
     {
         public int ID { get; set; }
         [Required]
+        [Display(Name = "Username")]
         public string username { get; set; }
         [Required]
+        [Display(Name = "Password")]
         public string password { get; set; }
+        [NotMapped]
+        [Compare("password")]
+        [Display(Name = "Confirm Password")]
+        public string confirmPassword { get; set; }
         [Required]
+        [Display(Name = "Full Name")]
         public string fullname { get; set; }
     }
 }
